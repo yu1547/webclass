e7509201c9c631bec9d2a34ba67422fd9b34d03b
yu1547 <ppeennnnyy1547@gmail.com>
2023-12-23 23:22:37 +0800

    login修改完成

--- nerdapp/routes/login.js
+++ nerdapp/routes/login.js
@@ -19,6 +19,7 @@ mongoose.connect('mongodb+srv://01157120:2R9cTuCe6gEDTcEe@nerds.m6vcm0c.mongodb.
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// 將 'dashboard.html' 的路由註冊到 login.js 中
 
 // 處理登入請求
 app.post('/login', async (req, res) => {
@@ -26,63 +27,66 @@ app.post('/login', async (req, res) => {
     if (!user) {
         return res.status(400).send('Invalid username or password.');
     }
-
+    
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) {
         return res.status(400).send('Invalid username or password.');
     }
     
     // 檢查 'tests' 陣列是否有元素
+    console.log(user.data.tests.length)
     if (user.data.tests.length > 0) {
-        // 如果有，則跳轉到 'calculate' 路由
-        res.redirect('/calculate');
+        // 如果有，則傳送 'calculate' 路由的 URL
+        res.json({ redirect: '/calculate' });
     } else {
-        // 如果沒有，則跳轉到 'dashboard.html'
-        res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+        // 如果沒有，則傳送 'dashboard.html' 的 URL
+        // res.json({ redirect: '../public/dashboard.html' });
+        res.json({ redirect: '/dashboard.html' });
+        
     }
-
-
-
+    
+    
+    
     // 在這裡進行運算
     // const result = performCalculation();
     // 這邊做計算，如果沒東西回傳空則前端會進到calculate
     // if(!user.a&&!user.b){
-    //     res.sendFile(path.join(__dirname, '../public/dashboard.html'));
-    // }
-    // if (user.a && user.b) {
-    //   const result = user.a + user.b;
-    //   res.send({ result: result });
-    // } else {
-    //   res.send({});
-    // }
-    // 將結果儲存到資料庫
-    // user.results.push(result);
-    // await user.save();
-
-
-    // res.send('Login successful!');
-});
-
-//處理計算請求並回傳結果
-app.post('/calculate', async (req, res) => {
-  const user = await User.findOne({ username: req.body.username }); // 你需要一種方法來獲取當前登入的用戶
-  user.a = req.body.a;
-  user.b = req.body.b;
-  await user.save();
-  const result = user.a + user.b;
+        //     res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+        // }
+        // if (user.a && user.b) {
+            //   const result = user.a + user.b;
+            //   res.send({ result: result });
+            // } else {
+                //   res.send({});
+                // }
+                // 將結果儲存到資料庫
+                // user.results.push(result);
+                // await user.save();
+                
+                
+                // res.send('Login successful!');
+            });
+            
+            //處理計算請求並回傳結果
+            app.post('/calculate', async (req, res) => {
+                const user = await User.findOne({ username: req.body.username }); // 你需要一種方法來獲取當前登入的用戶
+                user.a = req.body.a;
+                user.b = req.body.b;
+                await user.save();
+                const result = user.a + user.b;
   res.send({ result: result });
 });
 
 // 處理註冊請求
 app.post('/register', async (req, res) => {
     console.log("第一點")
-
+    
     // 檢查用戶名是否已經存在
     const existingUser = await User.findOne({ username: req.body.username });
     if (existingUser) {
         return res.status(400).send('Username already exists.');
     }    
-
+    
     
     let user = new User({
         username: req.body.username,
@@ -90,9 +94,12 @@ app.post('/register', async (req, res) => {
     });
     user = await user.save();
     console.log("第二點")
-
+    
     res.send(user);
 });
 
+app.get('/dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+});
 app.listen(3000, () => console.log('Server is running on port 3000...'));
 module.exports = router;
